3418682c7e8c0fb127a118f3fd50aba14d4592d9
suckamon <ginkin35@gmail.com>
2015-05-09 13:25:21 +0900

    gulp設定ファイルをコミット

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+var imagemin = require("gulp-imagemin");
+
+gulp.task("server", function(){
+	browser({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+gulp.task("sass", function(){
+	gulp.src("sass/**/*.scss")
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(gulp.dest("./css"))
+		.pipe(browser.reload({stream:true}));
+});
+
+gulp.task("js", function(){
+	gulp.src(["js/**/*.js", "!js/min/**/*.js"])
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(gulp.dest("./js/min"))
+		.pipe(browser.reload({stream:true}));
+});
+
+gulp.task("img", function(){
+	// gulp.src("img/**/*/(.png|.jpg|.gif)/")
+	gulp.src("img/*(*.jpg|*.png|*.gif)")
+		.pipe(imagemin())
+		.pipe(gulp.dest("./img/min"));
+});
+
+gulp.task("default", ['server'], function(){
+	gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
+	gulp.watch("sass/**/*.scss",["sass"]);
+	gulp.watch("img/*(*.jpg|*.png|*.gif)",["img"]);
+});	
