70a99fd7db9f1342be06076f9c491a24c9e8bb62
suckamon <ginkin35@gmail.com>
2015-08-08 14:06:27 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+/***************************************************
+* module load
+***************************************************/
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
@@ -7,63 +10,86 @@ var plumber = require("gulp-plumber");
 var imagemin = require("gulp-imagemin");
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var ejs = require('gulp-ejs');
 
+/***************************************************
+* path
+***************************************************/
+var root = "./";
+
+var cssSrcPath = root + "sass/**/*.scss";
+var cssDestPath = root + "css/";
+var jsSrcPath = root + "js/**/*.js";
+var jsDestPath = root + "js/min/";
+var imgSrcPath = root + "img/*(*.jpg|*.png|*.gif)";
+var imgDestPath = root + "img/min/";
+var ejsSrcPath = root + "**/*.ejs";
+var ejsDestPath = root + "html/";
+
+/***************************************************
+* tasks
+***************************************************/
 gulp.task("server", function(){
      browser({
-          notify: false
           server: {
-               baseDir: "./"
+               baseDir: root
           }
      });
 });
 
 gulp.task("html", function(){
-     gulp.src("**/*.html")
+     gulp.src(root + "**/*.html")
           .pipe(plumber())
           .pipe(browser.reload({stream:true}));
 });
 
 gulp.task("sass", function(){
-     gulp.src("sass/**/*.scss")
+     gulp.src(cssSrcPath)
           .pipe(plumber())
           .pipe(sass())
           .pipe(autoprefixer())
-          .pipe(gulp.dest("./css"))
+          .pipe(gulp.dest(cssDestPath))
           .pipe(browser.reload({stream:true}));
 });
 
 gulp.task("sass_watch", function(){
-     gulp.watch("sass/**/*.scss",["sass"]);
-     gulp.watch("css/**/*.css",["cssmin"]);
+     gulp.watch(cssSrcPath,["sass"]);
 });
 
 gulp.task('cssmin', function () {
-    gulp.src('css/*.css')
+    gulp.src(root + 'css/*.css')
         .pipe(cssmin())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./css/min'));
+        .pipe(gulp.dest(root + 'css/min'));
 });
 
 gulp.task("js", function(){
-     gulp.src(["js/**/*.js", "!js/min/**/*.js"])
+     gulp.src([jsSrcPath, "!" + root + "js/min/**/*.js"])
           .pipe(plumber())
           .pipe(uglify())
-          .pipe(gulp.dest("./js/min"))
+          .pipe(gulp.dest(jsDestPath))
           .pipe(browser.reload({stream:true}));
 });
 
 gulp.task("img", function(){
-     // gulp.src("img/**/*/(.png|.jpg|.gif)/")
-     gulp.src("img/*(*.jpg|*.png|*.gif)")
+     gulp.src(imgSrcPath)
           .pipe(imagemin())
-          .pipe(gulp.dest("./img/min"));
+          .pipe(gulp.dest(imgDestPath));
+});
+
+gulp.task("ejs", function() {
+    gulp.src(
+      [ejsSrcPath,'!' + root + "**/_*.ejs"]
+    )
+        .pipe(ejs())
+        .pipe(gulp.dest(ejsDestPath))
 });
 
 gulp.task("default", ['server'], function(){
-     gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
-     gulp.watch("sass/**/*.scss",["sass"]);
-     gulp.watch("img/*(*.jpg|*.png|*.gif)",["img"]);
-     gulp.watch("**/*.html", ["html"]);
+     gulp.watch([jsSrcPath, "!" + root + "js/min/**/*.js"],["js"]);
+     gulp.watch(cssSrcPath,["sass"]);
+     gulp.watch(imgSrcPath,["img"]);
+     gulp.watch(root + "**/*.html", ["html"]);
 });
